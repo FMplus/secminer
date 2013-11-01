1e7fee37c4f6306bb0714f58fe2cc0894bad327f
lessmoon <lsmcsp@gmail.com>
2013-11-01 19:56:29 +0800

    index of the web site
    It is a very simple version with nothing.

---
+++ src/index.php
@@ -0,0 +1,10 @@
+<html>
+    <form action="upload_profile.php" method="post"
+    enctype="multipart/form-data">
+        <label for="file">choose your new profile:</label>
+        <input type="file" name="file" id="file" /> 
+        <br />
+        <input type="submit" name="submit" value="Submit" />
+    </form>
+
+</html>
