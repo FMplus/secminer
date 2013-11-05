392ce6df954f6e2beef8032f72f1614139961328
lessmoon <lsmcsp@gmail.com>
2013-11-05 13:04:45 +0800

    No big change
    it is replaced by the file "signup.php"

--- src/register.php
+++ src/register.php
@@ -1,35 +1,22 @@
 <?php
-    session_start();
+    //session_start();
     include_once("config.php");
-    $con = mysql_connect(SM_HOST,SM_UID,SM_PSW);
-    if (!$con)
-      {
-        die('Could not connect: ' . mysql_error());
-      }
+    include_once("user_info_class.php");
+    if (isset($_GET['action']) && $_GET['action'] == 'submitted') {
+        $uid = $_GET['uid'];
+        $psw = $_GET['psw'];
+        $db_test = new db;
+        if(!$db_test -> open(SM_HOST,SM_DB,SM_UID,SM_PSW)){
+            die("Wrong : the database can't be open!");
+        }
+        
+        $sql = "insert into user_info(`uid`,`psw`)
+                values('$uid','$psw')";
 
-    $uid = $_GET['uid'];
-    $psw = $_GET['psw'];
-    mysql_select_db(SM_DB, $con);
-    $sql = "select count(*) as Existed from user_info
-            where ('$uid' = `uid`)";
-    $result = mysql_query($sql,$con);
-    if (!$result)
-    {
-        die('Error: ' . mysql_error());
+        if(!$db_test -> query($sql)){
+            die("Wrong : Can't insert into the database!");
+        }
+
+        $db_test -> close();
     }
-    $row = mysql_fetch_array($result);
-    $IsExisted = $row['Existed'];
-    if($IsExisted > 0)
-    {
-        die('Error: This user id has already existed!');
-    }
-    $sql = "INSERT INTO user_info(uid,psw) 
-            VALUES ('$uid', '$psw')";
-    if(!mysql_query($sql,$con))
-    {
-        die('Error: ' . mysql_error());
-    }
-    mysql_close($con);
-    $_SESSION['uid']    =   $uid;
-    $_SESSION['psw']    =   $psw;
 ?>
