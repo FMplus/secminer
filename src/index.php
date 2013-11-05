6ba6c21d64d9f2facc7188a3aef0651a8038a2bc
lessmoon <lsmcsp@gmail.com>
2013-11-05 13:03:46 +0800

    just changed to the test of the sign up page
    Sign up testing page

--- src/index.php
+++ src/index.php
@@ -1,10 +1,15 @@
-<html>
-    <form action="upload_profile.php" method="post"
-    enctype="multipart/form-data">
-        <label for="file">choose your new profile:</label>
-        <input type="file" name="file" id="file" /> 
-        <br />
-        <input type="submit" name="submit" value="Submit" />
-    </form>
-
-</html>
+<form action="signin.php" method="get">
+    name:  <input type="text" name="name"><br />
+    uid:  <input type="text" name="uid"><br />
+    psw:  <input type="password" name="psw"><br />
+    address:  <input type="text" name="addr"><br />
+    location: <input type="text" name="loc"><br />
+    profile:  <input type="file" name="pro"><br />
+    sex 
+        <input type = "radio" name = "gender" value = "male" >male
+        <input type = "radio" name = "gender" value = "female" >female
+        <input type = "radio" name = "gender" value = "unknown" >unknown
+    <br />
+    <input type="hidden" name="action" value="submitted" />
+    <input type="submit" name="submit" value="submit me!" />
+</form>
