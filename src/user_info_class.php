3517354a81551405f0b1afb2ff47fa1d8b623056
lessmoon <lsmcsp@gmail.com>
2013-11-05 13:08:21 +0800

    adding functions and fixed bugs(test passed)
    1.fixed some spelling error
    2.test passed

--- src/user_info_class.php
+++ src/user_info_class.php
@@ -2,13 +2,12 @@
     include_once("config.php");
 
     class user_info{
-        $name;
-        $id;
-        $profile;
-        $address;
-        $gender;
-        $location;
-        
+        public $name;
+        public $id;
+        public $profile;
+        public $address;
+        public $gender;
+        public $location;
         function __construct($name,$id,$profile,$address,$gender,$location){
             $this -> name    = $name;
             $this -> id      = $id;
@@ -17,47 +16,88 @@
             $this -> gender  = $gender;
             $this -> location= $location;
         }
+
         
        function __toString() { 
-            return "OAuthConsumer[key=$this->key,secret=$this->secret]"; 
-        } 
+            return "user_info[name      =   $this->name,
+                              id        =   $this->id,
+                              profile   =   $this->profile,
+                              address   =   $this->address,
+                              gender    =   $this->gender,
+                              location  =   $this->location]"; 
+        }
+    }
+        
+    class db{
+        private $db_con;
+        
+        function __construct(){
+            $this -> db_con = null;
+        }
+
+        function __destruct(){
+            $this -> close();
+        }
+        
+        function open($host,$dbn,$uid,$psw){
+            $this -> close();
+            $this -> db_con = mysql_connect($host,$uid,$psw);
+            if(!$this -> db_con){
+                return null;
+            }
+            mysql_select_db($dbn, $this -> db_con);
+            return $this ->db_con;
+        }
+
+        function is_open(){
+            return $this -> db_con?true:false;
+        }
+
+        function close(){
+            if($this -> db_con){
+                mysql_close($this -> db_con);
+                $this -> db_con = null;
+            }
+        }
+
+        function query($sql){
+            if(!$this -> db_con){
+                return null;
+            }
+            return mysql_query($sql,$this ->db_con);
+        }
     }
         
     class user_info_db{
-        $db_con;
+        private $sm_db;
+        private $lock;
 
         function __construct(){
-            $db_con = null;
+            $this -> sm_db = new db();
         }
         
+        function __destruct(){
+            $this -> sm_db -> close();
+        }
+
+        function lock(){
+            
+        }
+
         function open(){
-            if($db_con){
-                mysql_close($db_con);
-                $db_con = null;
-            }
-
-            $db_con = mysql_connect(SM_HOST,SM_UID,SM_PSW);
-
-            if(!$db_con){
-                return $db_con;
-            }
-            mysql_select_db(SM_DB, $con);
-            return $db_con;
+            return $this -> sm_db -> open(SM_HOST,SM_DB,SM_UID,SM_PSW);
         }
-        
+  
         function close(){
-            if($db_con){
-                mysql_close($db_con);
-                $db_con = null;
-            }
+            $this -> sm_db -> close();
         }
-        
+
         function search_uid($uid){
-            if(!$db_con)
+            if(!$this -> sm_db -> is_open())
                 return null;
-            $sql = "select count(*) as Existed from user_info
+            $sql = "select count(*) as Existed from user_info 
                     where ('$uid' = `uid`)";
-            $result = mysql_query($sql,$db_con);
+            $result = $this -> sm_db -> query($sql);
             if (!$result)
             {
                 die('Error: ' . mysql_error());
@@ -68,11 +108,11 @@
         }
         
         function check_uid_psw_ok($uid,$psw){
-            if(!$db_con)
+            if(!$this -> sm_db -> is_open())
                 return null;
-            $sql = "select count(*) as Existed from user_info
+            $sql = "select count(*) as Existed from user_info 
                     where ('$uid' = `uid` and '$psw' = `psw`)";
-            $result = mysql_query($sql,$db_con);
+            $result = $this -> sm_db -> query($sql);
             if (!$result)
             {
                 die('Error: ' . mysql_error());
@@ -81,16 +121,53 @@
             $IsExisted = $row['Existed'];
             return ($IsExisted > 0);
         }
-        
-        function fetch_user_info($uid,$psw){
-            if(!$db_con)
+
+        function add_user($user,$psw){
+            if(!$this -> sm_db -> is_open())
                 return null;
-            if(check_uid_psw_ok($uid,$psw)){
-                $uinfo = new user_info();
-                return $uinfo;
+            
+            $sql = "insert into user_info(`uid`,`name`,`psw`,`profile`,`address`,`gender`,`location`)
+                    values ('{$user->id}','{$user->name}','$psw',
+                    '{$user->profile}','{$user->address}','{$user->gender}','{$user->location}')";
+            $result = $this -> sm_db -> query($sql);
+            return $result;
+        }
+
+        function remove_user($uid){
+            if(!$this -> sm_db -> is_open())
+                return null;
+                
+            $sql = "delete from user_info 
+                    where '$uid' = `uid`";
+                    
+            $result = $this -> sm_db -> query($sql);
+            return $result;
+        }
+
+        function fetch_user_info($uid,$psw){
+            if(!$this -> sm_db -> is_open())
+                return null;
+            if(!$this -> check_uid_psw_ok($uid,$psw)){
+                die('Error: uname or password not match!');
             }
-            return null;
+            $sql   = "select name,id,profile,address,gender,location
+                      from user_info 
+                      where ('$uid' = `uid` and '$psw' = `psw`)";
+            
+            $result = $this -> sm_db -> query($sql);
+            $row = mysql_fetch_array($result);
+            
+            return new $user_info($row['name'],$row['id'],$row['profile'],$row['address'],$row['gender'],$row['location']);
+        }
+
+        function update_user_info($uid,$u_info){
+            if(!$this -> sm_db -> is_open())
+                return null;
+            $sql = "update user_info 
+                    set `name` = '{$user->name}',`profile` = '{$user->profile}', 
+                        `address` = '{$user->address}',`location` = '{$user->location}',`gender` = '{$user->gender}' 
+                    where `uid` = '$uid'";
+            $result = $this -> sm_db -> query($sql);        
+            return $result;
         }
     }
-    
-?>
