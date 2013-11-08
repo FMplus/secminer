807c59c0b6a5ffc64a65d46bdefbf343955a2969
lessmoon <lsmcsp@gmail.com>
2013-11-08 17:03:30 +0800

    finished sign up page
    1.check if it is already existed
    2.add the user

--- src/signup.php
+++ src/signup.php
@@ -2,17 +2,23 @@
     //session_start();
     include_once("config.php");
     include_once("user_info_class.php");
-    if (isset($_GET['action']) && $_GET['action'] == 'submitted') {
-        $user = new user_info(  $name       = $_GET['name'],
-                                $id         = $_GET['uid'],
-                                $profile    = "img/default_profile.bmp",
-                                $address    = $_GET['addr'],
-                                $gender     = $_GET['gender'],
-                                $location   = $_GET['loc']
-                                );
-        $user_db = new user_info_db();
-        $user_db -> open();
-        $user_db -> add_user($user,$_GET['psw']);
-        $user_db -> close();
-    }
+
+    $user = new user_info(    
+                              $id           = 0,//no effect
+                              $name         = $_POST['name'],
+                              $nickname     = $_POST['nickname'],
+                              $addr         = $_POST['addr'],
+                              $gender       = $_POST['gender'],
+                              $grade        = $_POST['grade'],
+                              $school       = $_POST['school'],
+                              $profile      = "img/profile.jpg",
+                              $phoneno      = $_POST['phoneno']
+                            );
+    
+    $user_db = new user_info_db();
+    $user_db -> open();
+    $user_db -> add_user($user,$_POST['password']);
+    $user_db -> close();
+
+    header("signin.html");
 ?>
