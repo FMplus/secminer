ede2413a937b2da5ec7b318e8da37653c72090bc
asq115266 <1152663905@qq.com>
2013-11-13 11:38:30 +0800

    goods_info
    The goods_info calss, you can add goods if you can have a goods object,
    remove a goods from table goods_info, search goods as name or id but not
    give the goods' images to buyer, if you want to scan the info of a goods
    you can fetch info as id or name images included. Besides, seller can
    change the goods' stock(quantity), state, descibe and price.

---
+++ src/goods_info_class.php
@@ -0,0 +1,261 @@
+<?php
+include_once("config.php");
+include_once("db_class.php");
+
+class goods_info{
+	public $id;
+	public $name;
+	public $usingdgr;
+	
+	public $originalprice;
+	public $currentprice;
+	public $dscrb;
+	
+	public $quantity;
+	public $state;
+	public $sid;
+	
+	public $photo ;
+	
+	function __construct($id,$name,$usingdgr,
+						 $originalprice,$currentprice,
+						 $dscrb,$quantity,$state,$sid)
+	{
+		$this -> id = $id;
+		$this -> name = $name;	
+		$this -> usingdgr = $usingdgr;
+		
+		$this -> originalprice = $originalprice;
+		$this -> currentprice = $currentprice;
+		$this -> dscrb = $dscrb;
+		
+		$this -> quantity = $quantity;
+		$this -> state = $state;
+		$this -> sid = $sid;
+		
+		$this -> photo = array();
+	}
+	
+	function __toString() { 
+			return "goods_info[id       => $this -> id,
+							   name     => $this -> name,
+                               usingdgr => $this -> usingdgr,
+                               
+                               origianlprice     => $this -> originalprice,
+                               currentprice   => $this -> currentprice,
+                               dscrb    => $this -> dscrb,
+
+                               quantity   => $this -> quantity,
+                               state  => $this -> state,
+                               sid  => $this -> sid]"; 
+        }
+}
+
+class goods_info_db
+{
+	private $sm_db;
+    private $lock;
+
+    function __construct(){
+        $this -> sm_db = new db();
+    }
+        
+    function __destruct(){
+         $this -> sm_db -> close();
+    }
+
+    function lock(){
+            
+    }
+	
+	function open(){
+        return $this -> sm_db -> open(SM_HOST,SM_DB,SM_UID,SM_PSW);
+    }
+  
+    function close(){
+        $this -> sm_db -> close();
+    }
+	
+	function search_gname($name){
+            if(!$this -> sm_db -> is_open())
+                return null;
+            $sql = "select count(*) as Existed from goods_info 
+                    where ('$name' = `name`)";
+            $result = $this -> sm_db -> query($sql);
+            if (!$result)
+            {
+				echo "fuck";
+               return NULL;
+            }
+            $row = mysql_fetch_array($result);
+            $IsExisted = $row['Existed'];
+			echo $IsExisted; 
+            return ($IsExisted > 0);
+    }
+		
+	function search_gid($id){
+            if(!$this -> sm_db -> is_open())
+                return null;
+            $sql = "select count(*) as Existed from goods_info 
+                    where ('$id' = `id`)";
+
+            $result = $this -> sm_db -> query($sql);
+            if (!$result)
+            {
+                return NULL;
+            }
+            $row = mysql_fetch_array($result);
+            $IsExisted = $row['Existed'];
+            return ($IsExisted > 0);
+    }
+	
+	function add_goods($goods){
+            if(!$this -> sm_db -> is_open())
+                return null;
+   
+            $sql = "insert into goods_info(`name`,`usingdgr`,`originalprice`,`currentprice`,`dscrb`,`quantity`,`state`,`sid`)
+                    values ('{$goods -> name}','{$goods -> usingdgr}','{$goods -> originalprice}',
+                    '{$goods -> currentprice}','{$goods -> dscrb}','{$goods -> quantity}','{$goods -> state}','{$goods -> sid}')";
+            $result = $this -> sm_db -> query($sql);
+            return $result;
+    }
+	
+	function remove_goods($id){
+            if(!$this -> sm_db -> is_open())
+                return null;
+                
+            $sql = "delete from goods_info 
+                    where '$id' = `id`";
+                    
+            $result = $this -> sm_db -> query($sql);
+            return $result;
+        }
+	
+	function fetch_goods_info_asname($name){
+            if(!$this -> sm_db -> is_open())
+                return null;
+            if(!$this -> search_gname($name)){
+                return null;
+            }
+            $sql   = "select *
+                      from  goods_info
+                      where '$name' = `name`";
+            
+            $result = $this -> sm_db -> query($sql);
+			if($result == null)
+			{
+				return null;
+			}
+			$row = mysql_fetch_object($result);
+			$goods = new goods_info($row -> id,$row ->name,$row ->usingdgr,
+						 $row ->originalprice,$row ->currentprice,
+						 $row ->dscrb,$row ->quantity,$row ->state,$row ->sid);
+			$id = $goods -> id;
+			$sql   = "select photo
+                      from  goods_photo
+                      where '$id' = `id`";
+			echo $sql;
+			echo '<br>';
+            $result = $this -> sm_db -> query($sql);
+			if($result == null)
+			{
+				return null;
+			}
+			while($row = mysql_fetch_array($result, MYSQL_ASSOC))
+			{
+				array_push($goods -> photo,$row);
+			}
+			return $goods;
+        }
+	
+	function fetch_goods_info_asid($id){
+            if(!$this -> sm_db -> is_open())
+                return null;
+            if(!$this -> search_gid($id)){
+                return null;
+            }
+            $sql   = "select *
+                      from  goods_info
+                      where '$id' = `id`";
+            
+            $result = $this -> sm_db -> query($sql);
+			if($result == null)
+			{
+				return null;
+			}
+			$row = mysql_fetch_object($result);
+			
+			$goods = new goods_info($row -> id,$row ->name,$row ->usingdgr,
+						 $row ->originalprice,$row ->currentprice,
+						 $row ->dscrb,$row ->quantity,$row ->state,$row ->sid);
+			
+			
+			$sql   = "select photo
+                      from  goods_photo
+                      where '$id' = `id`";
+            $result = $this -> sm_db -> query($sql);
+			if($result == null)
+			{
+				return null;
+			}
+			while($row = mysql_fetch_array($result, MYSQL_ASSOC))
+			{
+				array_push($goods -> photo,$row);
+			}
+			return $goods;
+        }
+		
+	function update_goods_quantity($id,$int){
+            if(!$this -> sm_db -> is_open())
+                return null;
+            $sql = "update goods_info 
+					set `quantity` = '$int'  
+					where `id` = '$id'";
+            $result = $this -> sm_db -> query($sql);
+            return $result;
+    }
+	
+	function update_goods_dscrb($id,$dscrb){
+            if(!$this -> sm_db -> is_open())
+                return null;
+            $sql = "update goods_info 
+					set `dscrb` = '$dscrb'
+					where '$id' = `id`";
+			echo $sql;
+            $result = $this -> sm_db -> query($sql);
+            return $result;
+    }
+	
+	function update_goods_currentprice($id,$price){
+            if(!$this -> sm_db -> is_open())
+                return null;
+            $sql = "update goods_info 
+					set `currentprice` = '$price'  
+					where `id` = '$id'";
+            $result = $this -> sm_db -> query($sql);
+            return $result;
+    }
+	
+	function update_goods_originalprice($id,$price){
+            if(!$this -> sm_db -> is_open())
+                return null;
+            $sql = "update goods_info 
+					set `originalprice` = '$price'  
+					where `id` = '$id'";
+            $result = $this -> sm_db -> query($sql);
+            return $result;
+    }
+	
+	function update_goods_state($id){
+            if(!$this -> sm_db -> is_open())
+                return null;
+            $sql = "update goods_info 
+					set `state` = 'sellout'  
+					where `id` = '$id' and `quantity` = '0'";
+            $result = $this -> sm_db -> query($sql);
+            return $result;
+    }
+}
+
+?>
+
