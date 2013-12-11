48bc8093e1f0468220a5131d6ccf4284ff41ea94
fmplus <597621139@qq.com>
2013-12-11 23:58:11 +0800

    Create addcomment.php

---
+++ src/addcomment.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+include_once("comment.php");
+$comment = new comment_info($id = null,
+							$time = null,
+							$content = $_POST['content'],
+							$oid     = $_POST['oid']);
+print_r($comment);
+$cdb = new comment_info_db;
+$cdb -> open();
+$cdb -> add_comment_asoid($comment -> oid,$comment -> content);
+$cdb -> close();
+echo("评论添加成功");
+?>
