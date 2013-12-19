d198a658c86380cbcd434b456a9b70d08482a3ce
fmplus <597621139@qq.com>
2013-12-20 00:54:19 +0800

    Update config.php

--- src/config.php
+++ src/config.php
@@ -1,6 +1,10 @@
 <?php
     define( "SM_HOST" , 'localhost' );
     define( "SM_UID" ,  'root' );
-    define( "SM_PSW" ,  '1252604' );
+    define( "SM_PSW" ,  '' );
     define( "SM_DB" ,  'secminer' );
-?>
+    define( "PATH_SM" ,  '/' );
+    define( "PATH_PROFILE" , 'profile/' );
+    define( "PATH_IMG" ,'img/' );
+    define( "DEFAULT_PROFILE" ,PATH_IMG.'profile.jpg' );
+	define( "PATH_GDIMG" ,'gdimg/' );
