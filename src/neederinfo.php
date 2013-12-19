0a55cfe9854bb8986b831cc2ac8a492fc3c25449
fmplus <597621139@qq.com>
2013-12-20 01:00:54 +0800

    show the needer's person info

---
+++ src/neederinfo.php
@@ -0,0 +1,60 @@
+<?php 
+	session_start();
+	include_once 'user_info_class.php';
+	$user_db = new user_info_db();
+	$user_db -> open();
+	$user_info = $user_db -> fetch_user_info($_GET['nid']);
+	$user_db -> close();
+?>
+
+<html>
+<head>
+<meta charset="UTF-8"/>
+<link rel="stylesheet" type="text/css" href="css/infocenter.css" >
+</head>
+<body>
+
+<div id="userinfo">
+<form >
+  <fieldset>
+    <legend>用户 <?= $user_info -> name?> 的信息</legend>
+	<br/>
+	用户头像:
+	<img src="<?= $user_info -> profile ?>" height = '100px' width = '100px'/>
+	<br/><br/>
+		
+    昵称：
+	<?= $user_info -> nickname ?>
+	<br/><br/>
+	
+	用户名：
+	<?= $user_info -> name ?>
+	<br/><br/>
+	
+	性别:
+	   <?php if($user_info -> gender == 'm'){?>男
+	   <?php }else if($user_info -> gender == 'f'){?>女
+	   <?php }else{?>保密<?php }?>
+	<br/><br/>
+	年级:
+	<?= $user_info -> grade ?>
+	<br/><br/>
+	
+	学校:
+	<?= $user_info -> school ?>
+	<br/><br/>
+	
+	公寓地址:
+	<?= $user_info -> addr ?>
+	<br/><br/>
+	
+	联系电话：
+	<?= $user_info -> phoneno ?>
+	<br/><br/>
+	</fieldset>
+
+</form>
+</div>
+
+</body>
+</html>
