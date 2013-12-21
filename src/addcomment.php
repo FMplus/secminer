8d0dd4e5bf76b7cc41f555e3ed2069bea8fe7804
fmplus <597621139@qq.com>
2013-12-22 00:48:13 +0800

    Update addcomment.php

--- src/addcomment.php
+++ src/addcomment.php
@@ -5,10 +5,10 @@ $comment = new comment_info($id = null,
 							$time = null,
 							$content = $_POST['content'],
 							$oid     = $_POST['oid']);
-print_r($comment);
+//print_r($comment);
 $cdb = new comment_info_db;
 $cdb -> open();
 $cdb -> add_comment_asoid($comment -> oid,$comment -> content);
 $cdb -> close();
-echo("评论添加成功");
+echo("评论成功");
 ?>
