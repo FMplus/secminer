622fa7ef29cb99e8cd349a7328a9f35449cf6ade
fmplus <597621139@qq.com>
2013-12-22 00:38:36 +0800

    Update upload_profile.php

--- src/upload_profile.php
+++ src/upload_profile.php
@@ -1,35 +1,23 @@
 <?php
-    
-    if ((($_FILES["file"]["type"] == "image/gif")
-    || ($_FILES["file"]["type"] == "image/jpeg")
-    || ($_FILES["file"]["type"] == "image/pjpeg"))
-    && ($_FILES["file"]["size"] < 2*1024*1024))
-      {
-      if ($_FILES["file"]["error"] > 0)
-        {
-        echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
-        }
-      else
-        {
-        echo "Upload: " . $_FILES["file"]["name"] . "<br />";
-        echo "Type: " . $_FILES["file"]["type"] . "<br />";
-        echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-        echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
-
-        if (file_exists("profile/" . $_FILES["file"]["name"]))
+    include_once("config.php");
+    function upload_profile($file,$newname){
+       if ((($file["type"] == "image/gif")
+            || ($file["type"] == "image/jpeg")
+            || ($file["type"] == "image/pjpeg"))
+            && ($file["size"] < 2*1024*1024))
           {
-          echo $_FILES["file"]["name"] . " already exists. ";
+              if ($file["error"] > 0)
+                {
+                    return 0;
+                }
+              else
+                {
+                    move_uploaded_file($file["tmp_name"],PATH_PROFILE."$newname");
+                    return 1;
+                }
           }
         else
           {
-          move_uploaded_file($_FILES["file"]["tmp_name"],
-          "profile/" . $_FILES["file"]["name"]);
-          echo "Stored in: " . "profile/" . $_FILES["file"]["name"];
+            return 0;
           }
-        }
-      }
-    else
-      {
-      echo "Invalid file";
-      }
-?>
+    }
